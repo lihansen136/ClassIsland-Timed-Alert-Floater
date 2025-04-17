d31df6c7b4c8d655218d3d103e24e70bfe91e9ef
lihansen136 <1640884306@qq.com>
2025-04-17 19:18:35 +0800

    feat: 添加自定义倒计时插件功能
    添加了自定义倒计时插件的基本功能，包括倒计时窗口、输入框、开始按钮以及音频提示功能。插件使用WPF实现，支持倒计时结束时的音频播放，并集成了日志记录功能。

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System;
+
+namespace ClassIsland.CustomCountdownPlugin
+{
+    using System;
+    public class Program
+    {
+        [System.STAThread]
+        public static void Main(string[] args)
+        {
+            var plugin = new ClassIsland.CustomCountdownPlugin.Plugin();
+        }
+    }
+}
